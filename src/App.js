90871e24933c75cb7d9a787e502f3ef6fefdb5b6
Shahbaz Ali <38136101+shahbaz-bandits25@users.noreply.github.com>
2020-08-04 16:57:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO WORLD IN REACT
+          HELLO WORLD IN REACT By SHAHBAZ
         </p>
         <a
           className="App-link"
